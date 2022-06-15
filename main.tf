be6bc3ac64baa5a84d1701922087e9f3091e1f16
Roman Savchuk <rsavchu@softserveinc.com>
2022-06-15 15:45:22 +0300

    updated backend

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 terraform {
 
+  backend "s3" {
+    bucket = "my-automation-server-backend"
+    key    = "automation_server/web_servers_infrastructure.tfstate"
+    region = "eu-central-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
